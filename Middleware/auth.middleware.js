95fd5221d01b9cdd0c37471f26f79d657eb34c76
Jian <jianhui9998@gmail.com>
2022-07-31 17:53:03 +0800

    update response

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -1,18 +1,15 @@
 const jwt = require("jsonwebtoken");
+const {successResponse, errorResponse } = require('../Utils/apiResponse')
 
 const checkAuth = (req, res, next)  => {
     const token = req.header("auth-token");
-    if (!token) return res.status(400).send({
-        message: "Access Denied!, no token entered"
-    });
+    if (!token) return res.status(400).send(errorResponse("Access Denied!, no token entered"));
     try {
         const verified = jwt.verify(token, process.env.jwtSecret);
         req.user = verified;
         next();
     } catch (err) {
-        res.status(400).send({
-            error: "auth failed"
-        });
+        res.status(400).send(errorResponse("auth failed"));
     }
 };
 
